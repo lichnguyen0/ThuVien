ca8552d71f954299b49d53facbf6ba0ed0547b1e
lichnguyen0 <fakesangmy@gmail.com>
2025-07-14 22:07:58 +0700

    update

---
+++ src/tirnkhaiinterfacedesosanh/CircleComparatorTest.java
@@ -0,0 +1,26 @@
+package tirnkhaiinterfacedesosanh;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CircleComparatorTest {
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(3.6);
+        circles[1] = new Circle(); // radius = 1.0
+        circles[2] = new Circle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Comparator<Circle> circleComparator = new CircleComparator(); // ✅ Dùng generic
+        Arrays.sort(circles, circleComparator);
+
+        System.out.println("After-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
