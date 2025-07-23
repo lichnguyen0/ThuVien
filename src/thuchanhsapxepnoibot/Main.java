0710ac4a30a65070e79ec4871bdf01107b986669
lichnguyen0 <fakesangmy@gmail.com>
2025-07-23 09:23:20 +0700

    update

---
+++ src/thuchanhsapxepnoibot/Main.java
@@ -0,0 +1,9 @@
+package thuchanhsapxepnoibot;
+
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort.bubbleSort(BubbleSort.list);
+        for (int i = 0; i < BubbleSort.list.length; i++)
+            System.out.print(BubbleSort.list[i] + " ");
+    }
+}
