64705cbca698af64dab7d8a0a760c2042f2f5dcf
lichnguyen0 <fakesangmy@gmail.com>
2025-08-10 18:43:46 +0700

    update

---
+++ src/trienkhaistrategy/SelectionSort.java
@@ -0,0 +1,11 @@
+package trienkhaistrategy;
+
+import java.util.List;
+
+public class SelectionSort implements SortStrategy{
+
+    @Override
+    public <T> void sort(List<T> items) {
+        System.out.println("Selection sort");
+    }
+}
---
+++ src/trienkhaistrategy/SortStrategy.java
@@ -0,0 +1,8 @@
+package trienkhaistrategy;
+
+import java.util.List;
+
+public interface SortStrategy {
+
+    <T> void sort(List<T> items);
+}
